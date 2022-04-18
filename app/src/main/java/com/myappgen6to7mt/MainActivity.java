35b9e2090893b3090a70167ba1425bb68802ecff
Aarti Patel <royal.arati10@gmail.com>
2022-04-18 18:44:51 +0530

    Java Findviewby ID

--- app/src/main/java/com/myappgen6to7mt/MainActivity.java
+++ app/src/main/java/com/myappgen6to7mt/MainActivity.java
@@ -3,12 +3,33 @@ package com.myappgen6to7mt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtUserName;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUserName = findViewById(R.id.edt_username);
+        btnLogin = findViewById(R.id.btn_login);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strUserName = edtUserName.getText().toString();
+                Toast.makeText(MainActivity.this, "UserName is " + strUserName, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
