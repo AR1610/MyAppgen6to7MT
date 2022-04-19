e0b1d0e2c0e4724c97621b9785e25c9e1737cddb
Aarti Patel <royal.arati10@gmail.com>
2022-04-19 18:59:49 +0530

    Type of intent Explicit and Implicit

---
+++ app/src/main/java/com/myappgen6to7mt/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.myappgen6to7mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView tvUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        tvUsername = findViewById(R.id.tv_username);
+
+        Intent i = getIntent();
+        String strUsername = i.getStringExtra("KEY_USERNAME");
+        tvUsername.setText("Welcome " + strUsername);
+
+
+    }
+}
--- app/src/main/java/com/myappgen6to7mt/MainActivity.java
+++ app/src/main/java/com/myappgen6to7mt/MainActivity.java
@@ -2,18 +2,22 @@ package com.myappgen6to7mt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtUserName;
-    Button btnLogin;
     TextView tvUsername;
+    ImageView imageView;
+    Button btnLogin,btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,19 @@ public class MainActivity extends AppCompatActivity {
         edtUserName = findViewById(R.id.edt_username);
         btnLogin = findViewById(R.id.btn_login);
         tvUsername = findViewById(R.id.tv_login);
+        imageView = findViewById(R.id.img_logo);
+        btnSend = findViewById(R.id.btn_send);
 
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.google.com"));
+              startActivity(i);
+
+
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +47,16 @@ public class MainActivity extends AppCompatActivity {
 
                 String strUserName = edtUserName.getText().toString();
                 tvUsername.setText(strUserName);
+                imageView.setImageResource(R.drawable.ic_person);
                 Toast.makeText(MainActivity.this, "UserName is " + strUserName, Toast.LENGTH_SHORT).show();
+
+                // Explicit Intent
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                i.putExtra("KEY_USERNAME",strUserName);
+                startActivity(i);
+                // over Explicit Intent
+
+
             }
         });
 
