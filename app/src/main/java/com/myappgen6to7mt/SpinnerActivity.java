6a58047b910af68daab5d85c260f0bf6a18d6501
Aarti Patel <royal.arati10@gmail.com>
2022-06-20 18:24:44 +0530

    SpinnerActivity with custom color

--- app/src/main/java/com/myappgen6to7mt/SpinnerActivity.java
+++ app/src/main/java/com/myappgen6to7mt/SpinnerActivity.java
@@ -1,9 +1,13 @@
 package com.myappgen6to7mt;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -13,7 +17,9 @@ public class SpinnerActivity extends AppCompatActivity {
 
     Spinner spinner;
     TextView tvSpinner;
-    String strLang[] = {"Android","java",".net","PHP","Flutter","IOS","Python"};
+
+
+   String strLang[] = {"Select item","Android","java",".net","PHP","Flutter","IOS","Python"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +29,32 @@ public class SpinnerActivity extends AppCompatActivity {
         spinner = findViewById(R.id.spinner);
         tvSpinner = findViewById(R.id.tv_selected_item);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,strLang);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,strLang){
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                TextView tvData = (TextView)  super.getDropDownView(position, convertView, parent);
+                if (position == 0){
+                    tvData.setTextColor(Color.GRAY);
+                }else {
+                    tvData.setTextColor(Color.BLUE);
+                }
+                return tvData;
+            }
+        };
         spinner.setAdapter(arrayAdapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                String strData = parent.getItemAtPosition(position).toString();
-                tvSpinner.setText("Selected item is "+strData);
+                if (position != 0){
+                    String strData = parent.getItemAtPosition(position).toString();
+                    tvSpinner.setText("Selected item is "+strData);
+                }
+
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
